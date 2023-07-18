a9d56b707539fee748a15a1d747f68b1aed48dc3
AnanthkrishnanJM <ananthkrishnan2101@gmail.com>
2023-07-18 19:06:23 +0530

    initial commit

---
+++ Countsum.java
@@ -0,0 +1,25 @@
+package columnsum;
+import java.util.Scanner;
+
+public class Countsum {
+	
+		public static void main(String[] args) {
+	
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		int ar[][]=new int[a][b];
+		for(int i=0;i<a;i++) {
+			for(int j=0;j<b;j++) {
+				ar[i][j]=sc.nextInt();
+			}
+		}
+		  for(int i = 0; i < b; i++){    
+	          int   sumCol = 0;    
+	            for(int j = 0; j < a; j++){    
+	              sumCol = sumCol + ar[j][i];    
+	            }    
+	            System.out.println( sumCol);    
+	        }    
+		}
+}
